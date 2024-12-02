8137fb136eaab4956e4ca2bcebe350ec7381e10d
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-12-02 15:14:31 +0300

    connect to database

--- farm/farm/settings.py
+++ farm/farm/settings.py
@@ -78,8 +78,15 @@ WSGI_APPLICATION = "farm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'farm',
+        "USER":'root',
+        "PASSWORD":'',
+        "HOST":'localhost',
+        "PORT":'3306',
+          'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'"
+        },
     }
 }
 
