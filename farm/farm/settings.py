88df4ce113571533a60be52faf062ce1c64e2a2d
quiesscent <ephesianslewis@gmail.com>
2024-12-08 19:49:58 +0300

    Migrations

--- farm/farm/settings.py
+++ farm/farm/settings.py
@@ -32,14 +32,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'farmers',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'authentication',
+    "authentication",
+    "main"
 ]
 
 MIDDLEWARE = [
